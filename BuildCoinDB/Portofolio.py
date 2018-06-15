579d03b2fd10fe6c688a276fd181f914b0f41d83
kduigou <kevin.k.duigou@gmail.com>
2018-06-15 19:16:10 +0200

    Add a mean to purge the DB

--- BuildCoinDB/Portofolio.py
+++
@@ -1,9 +0,0 @@
-
-
-class Portofolio():
-    def __init__(self):
-        self.ownerName = ""
-        self.content = {}
-
-    def addValue(self,currencyName: str,quantity : float):
-        self.content[currencyName] = quantity
